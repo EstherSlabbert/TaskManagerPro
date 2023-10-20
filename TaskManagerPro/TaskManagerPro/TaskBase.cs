d3bdbff73964752decdde22c319d78467f34029b
Esther Slabbert <super.ejs@gmail.com>
2023-10-20 18:52:25 +0100

    #1 Initial file structure and added TaskBase class

---
+++ TaskManagerPro/TaskManagerPro/TaskBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TaskManagerPro
+{
+    internal class TaskBase
+    {
+        public int TaskId { get; set; }
+        public string TaskTitle { get; set; }
+        public string TaskDescription { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool TaskIsComplete { get; set; }
+
+        public TaskBase(int id, string title, string description, DateTime dueDate, bool taskCompleted = false)
+        {
+            TaskId = id;
+            TaskTitle = title;
+            TaskDescription = description;
+            DueDate = dueDate;
+            TaskIsComplete = taskCompleted;
+        }
+    }
+}
