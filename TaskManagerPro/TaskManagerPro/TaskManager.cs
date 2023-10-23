1a50ecfa97afbaf21748158e72bb078e26237eed
Esther Slabbert <super.ejs@gmail.com>
2023-10-23 10:03:57 +0100

    #5 - Task Manager AddTask() + unit test

---
+++ TaskManagerPro/TaskManagerPro/TaskManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace TaskManagerPro
+{
+    public class TaskManager
+    {
+        public List<TaskBase> Tasks = new List<TaskBase>();
+        public int CurrentTaskId;
+
+        public void AddTask(TaskBase newTask)
+        {
+            Tasks.Add(newTask);
+        }
+    }
+}
---
+++ TaskManagerPro/TaskManagerProTests/TaskManagerUnitTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TaskManagerPro;
+
+namespace TaskManagerProTests
+{
+    [TestClass]
+    public class TaskManagerUnitTests
+    {
+        [TestMethod]
+        public void AddTask_ShouldAddTaskToTaskList()
+        {
+            // Arrange
+            TaskManager taskManager = new TaskManager();
+            taskManager.CurrentTaskId = 0;
+            TaskBase task = new TaskBase(1, "Sample task", "Sample description", DateTime.Now);
+
+            // Act
+            taskManager.AddTask(task);
+
+            // Assert
+            Assert.IsTrue(taskManager.Tasks.Contains(task));
+        }
+    }
+}
